409e53d10a4da4d909431cfbed4cd9daf0765a2b
littlease <little.ease@gmail.com>
2022-07-03 12:17:06 +0300

    Update smtp_to_telegram.go

--- smtp_to_telegram.go
+++ smtp_to_telegram.go
@@ -214,7 +214,7 @@ func main() {
 				"The maximum text file size is determined by `forwarded-attachment-max-size`.",
 			Value:   4095,
 			EnvVars: []string{"ST_MESSAGE_LENGTH_TO_SEND_AS_FILE"},
-		&cli.intFlag{
+		&cli.IntFlag{
 			Name:    "smtp-timeout-seconds",
 			Usage:   "SMTP timeout",
 			Value:   60,
